b1a68e4d2440cf7b798f655bb519a8569125850d
lyonmu <lyonmu@foxmail.com>
2026-06-20 12:24:08 +0800

    fix(id): 传播 ID 生成错误

--- id/sony_flake.go
+++ id/sony_flake.go
@@ -1,17 +1,17 @@
 package id
 
-import (
-	"time"
-
-	sonyflakeV2 "github.com/sony/sonyflake/v2"
-)
+import sonyflakeV2 "github.com/sony/sonyflake/v2"
 
 type IDGenerator interface {
-	GenID() int64
+	GenID() (int64, error)
+}
+
+type idNode interface {
+	NextID() (int64, error)
 }
 
 type SonySnowFlake struct {
-	node *sonyflakeV2.Sonyflake
+	node idNode
 }
 
 func NewSonySnowFlake(machineId func() (int, error)) (IDGenerator, error) {
@@ -22,8 +22,7 @@ func NewSonySnowFlake(machineId func() (int, error)) (IDGenerator, error) {
 		BitsMachineID: 0,
 		// 时间粒度，控制时间位的步长；0 表示默认 10ms，必须 ≥1ms
 		TimeUnit: 0,
-		// 自定义起始时间，ID 时间部分以该时间为基准；0 表示使用默认 2025-01-01 00:00:00 +0000 UTC，且必须早于当前时间
-		StartTime: time.Now(),
+		// 起始时间留空，使用 Sonyflake 固定默认值 2025-01-01 00:00:00 +0000 UTC
 		// 生成当前实例机器 ID 的函数；若返回错误，实例创建失败；留空则默认使用私有 IP 的低 16 位
 		MachineID: machineId,
 		// 校验机器 ID 唯一性的函数；返回 false 则实例不会创建；留空则不做校验
@@ -38,7 +37,6 @@ func NewSonySnowFlake(machineId func() (int, error)) (IDGenerator, error) {
 	return &SonySnowFlake{node: node}, nil
 }
 
-func (s *SonySnowFlake) GenID() int64 {
-	v, _ := s.node.NextID()
-	return v
+func (s *SonySnowFlake) GenID() (int64, error) {
+	return s.node.NextID()
 }
